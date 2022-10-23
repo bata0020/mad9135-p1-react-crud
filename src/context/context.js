8efd3ee741b5eb4e4a909d32ec92574972643b29
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-10-23 07:04:15 -0400

    Updated context file to use reducer and localstorage

--- src/context/context.js
+++ src/context/context.js
@@ -1,9 +1,20 @@
 import React, { createContext, useReducer, useEffect } from "react";
+import reducer from "../reducer/reducer";
 
 const DataContext = createContext();
 
 function DataProvider(props) {
-  return <DataContext.Provider value={[]} {...props} />;
+  const [cars, dispatch] = useReducer(reducer, [], () => {
+    const localStorageData = localStorage.getItem("carList");
+    return localStorageData ? JSON.parse(localStorageData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("carList", JSON.stringify(cars));
+  }, [cars]);
+
+  //TODO: verify the value format of return statement below
+  return <DataContext.Provider value={[cars, dispatch]} {...props} />;
 }
 
 export { DataContext, DataProvider };
