0e14ddc1fe78c88c7c364a04137780034c80218f
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-10-23 06:37:52 -0400

    Add Header component and folder structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>
+        <a href="/">React Crud</a>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
